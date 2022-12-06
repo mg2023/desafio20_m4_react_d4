c1f143d13c00adb8ce6ac99174f18da003323867
mg <amg.vergara@gmail.com>
2022-12-06 14:29:46 -0300

    Se muestra el listado de productos

--- src/components/AllProducts.jsx
+++ src/components/AllProducts.jsx
@@ -8,12 +8,12 @@ function AllProducts() {
     // 2. LLamamos al función que consume la API al momento de montar el    componente
     useEffect(() => {
         consultarProductos();
-      }, []);
+    }, []);
 
 
     // 1. Función que consulta la API
     const consultarProductos = async () => {
-        const url = 'https://dummyjson.com/products?limit=10&select=title,price,thumbnail,category';
+        const url = 'https://dummyjson.com/products?limit=30&select=title,price,stock,category,rating';
         const response = await fetch(url)
         const data = await response.json()
         setTablaProductos(data.products);
@@ -22,12 +22,34 @@ function AllProducts() {
 
     return (
         <div>
-            {/* { 4. Mostramos la info } */}   
-                <h2>Productos</h2>
-                 <ul className="list-group">
-                
-                {tablaProductos.map(x => <li className="list-group-item" key={x.id}>Catergoria es {x.category} </li> )  } 
-              </ul>
+            {/* { 4. Mostramos la info } */}
+            <h2>Productos</h2>
+            <table className="table table-sm table-bordered">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Price</th>
+                        <th>Stock</th>
+                        <th>Category</th>
+                        <th>Rating</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    {tablaProductos && 
+                    tablaProductos.map((producto)=>(
+                        <tr key={producto.id}>
+                            <td>{producto.title}</td>
+                            <td>{producto.price}</td>
+                            <td>{producto.stock}</td>
+                            <td>{producto.category}</td>
+                            <td>{producto.rating}</td>
+                        </tr>
+                    )  )}
+
+
+                </tbody>
+            </table>
         </div>
     );
 }
