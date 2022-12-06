6fc25f6088eca78dce2d40da1d33c3d7a1979b89
mg <amg.vergara@gmail.com>
2022-12-06 16:01:41 -0300

    Se agregan 2 botones para ordenar alfabeticamente

--- src/components/AllProducts.jsx
+++ src/components/AllProducts.jsx
@@ -9,8 +9,8 @@ function AllProducts() {
 
     const handleChange = (e) => {
         setBusqueda(e.target.value);
-        filtrar(e.target.value);    }
-
+        filtrar(e.target.value);
+    }
 
     const filtrar = (terminoBusqueda) => {
         let resultadosBusqueda = tablaProductos.filter((elemento) => {
@@ -28,7 +28,6 @@ function AllProducts() {
         consultarProductos();
     }, []);
 
-
     // 1. Función que consulta la API
     const consultarProductos = async () => {
         const url = 'https://dummyjson.com/products?limit=30&select=title,price,stock,category,rating';
@@ -39,6 +38,14 @@ function AllProducts() {
         console.log(data.products)
     }
 
+    const ordenardeAaZ = () => {
+        console.log("Ordenando de A a Z")
+    }
+
+    const ordenardeZaA = () => {
+        console.log("Ordenando de Z a A")
+    }
+
     return (
         <div>
             {/* { 4. Mostramos la info } */}
@@ -46,29 +53,43 @@ function AllProducts() {
             <div className="input-group mb-3 mt-3">
                 {/* <label for="exampleInputName" class="form-label"></label> */}
                 <input type="search" className="form-control" id="exampleInputName"
-                  placeholder="Busca por titulo o categoria"
-                  name="busqueda"
-                  onChange={handleChange}
-                  value={busqueda}
+                    placeholder="Busca por titulo o categoria"
+                    name="busqueda"
+                    onChange={handleChange}
+                    value={busqueda}
                 ></input>
                 <button type="submit" className="btn btn-primary">Buscar</button>
-              </div>
+            </div>
+            <div className="container text-center p-3">
+                <div className="row">
+                    <div className="col">
+                    <button type="submit" className="btn btn-primary" onClick={ordenardeAaZ}> A --> Z </button>
+                    </div>
+                    <div className="col">
+                    <button type="submit" className="btn btn-primary" onClick={ordenardeZaA} >Z --> A</button>
+                    </div>
+                </div>
+            </div>
 
-            { productos.length > 0?
-            <table className="table table-sm table-bordered">
-                <thead>
-                    <tr>
-                        <th>Title</th>
-                        <th>Price</th>
-                        <th>Stock</th>
-                        <th>Category</th>
-                        <th>Rating</th>
-                        <th>Buy</th>
+            
+            
 
-                    </tr>
-                </thead>
-                <tbody>
-                    {productos.map((producto) => (
+
+            {productos.length > 0 ?
+                <table className="table table-sm table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Price</th>
+                            <th>Stock</th>
+                            <th>Category</th>
+                            <th>Rating</th>
+                            <th>Buy</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {productos.map((producto) => (
                             <tr key={producto.id}>
                                 <td>{producto.title}</td>
                                 <td>{producto.price}</td>
@@ -78,12 +99,12 @@ function AllProducts() {
                                 <td><Button variant="primary">Buy</Button></td>
                             </tr>
                         ))
-                    }
-                       
-                </tbody>
-            </table>
-            :
-            <h3>El producto o categoría no ha sido encontrado </h3>
+                        }
+
+                    </tbody>
+                </table>
+                :
+                <h3>El producto o categoría no ha sido encontrado </h3>
             }
         </div>
     );
--- src/components/products.jsx
+++ src/components/products.jsx
@@ -8,9 +8,6 @@ function Products(props) {
             <Card.Img variant="top" src={props.image} />
             <Card.Body>
                 <Card.Title>{props.title}</Card.Title>
-                {/* <Card.Text>
-                    {props.descrip}
-                </Card.Text> */}
                 <Button variant="primary">Comprar</Button>
             </Card.Body>
         </Card>
